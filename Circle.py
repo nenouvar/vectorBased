153c2fcf3fc1b819591849194b1204733fc45f09
Laufer4u <nzevgolis4u@gmail.com>
2021-02-25 19:59:07 +0200

    class Circle Created
    Circle using vector, r and theta, to initialize

--- Circle.py
+++ Circle.py
@@ -2,5 +2,19 @@ import math
 from Vector import Vector
 class Circle:
     def __init__(self, center, r, theta):
+        self.center = center
+        self.r = r
+        self.theta = theta
+        self.n = len(self.center.list)
     def param(self): # vector-like object
+        param = []
+        x = self.center.list[0] + self.r*math.cos(self.theta)
+        y = self.center.list[1] + self.r*math.sin(self.theta)
+        param = [x, y]
+        return param
     def getPoints(self, n): # list of vectors
+        points = []
+        for i in range(n+1):
+            self = Circle(self.center, self.r, 2*i*math.pi/n)
+            points.append(self.param())
+        return points
--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 from Vector import Vector
 from Line import Line
-#from Circle import Circle
+from Circle import Circle
 
 # Vectors using lists
 e0 = Vector([0, 0])
 e1 = Vector([1, 0])
 e2 = Vector([0, 1])
 
-e3 = e1.add(e2)
-e4 = e1.sub(e2)
-d0 = e1.inn(e2)
-d1 = e1.inn(e3)
+# e3 = e1.add(e2)
+# e4 = e1.sub(e2)
+# d0 = e1.inn(e2)
+# d1 = e1.inn(e3)
 
 # Line using list of vectors and t
-l1 = Line([e0, e1], 2)
-print(l1.param)
-print(l1.getPoints(20))
-result = [e3.list, e4.list, d0, d1, l1.param.list]
-print('Your '+str(len(result))+' calculations')
-print('led to '+str(result)+' results.')
+# l1 = Line([e0, e1], 2)
+# print(l1.param)
+# print(l1.getPoints(20))
+# result = [e3.list, e4.list, d0, d1, l1.param.list]
+# print('Your '+str(len(result))+' calculations')
+# print('led to '+str(result)+' results.')
 
 # Circle using vector, r and theta
-#center = e0
-#c1 = Circle(center, 1, 0)
-#print(c1.param())
-#print(Vector(c1.param()))
-#print(c1.getPoints(20))
+center = e0
+c1 = Circle(center, 1, 0)
+print(c1.param())
+print(Vector(c1.param()))
+print(c1.getPoints(20))
